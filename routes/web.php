659d51681a0614d2e46d0f646e4ce6b3f1acee0f
E X O R C I S T <payakumbuh110@gmail.com>
2024-05-18 20:17:15 +0700

    Landing Page

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::get('/home', function () {
     ]);
 })->middleware(['auth', 'verified'])->name('dashboard');;
 
+Route::get('/welcome', function(){
+    return view('welcome');
+});
+
 //Route::get('/dashboard', function () {
 //    return Inertia::render('Dashboard');
 //})->middleware(['auth', 'verified'])->name('dashboard');
