de1a0bdb6e1b2438348e31214f6603b74f9ab1cf
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-05-30 11:06:42 +0700

    fix(jadwal): manage conflict formatting

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -15,16 +15,16 @@ class JadwalController extends Controller
 
         $thead = ['Nomor', 'Hari', 'Waktu', 'Ruangan'];
 
-        $tbody = $user->map(function ($item) {
+        $tbody = $jadwal->map(function($item){
             return [
                 'id_user' => $item->id_user,
                 'hari' => $item->hari,
                 'waktu_mulai' => $item->waktu_mulai,
                 'ruangan' => $item->nama_ruangan,
-            ];
+                ];
         });
 
-        return Inertia::render('Jadwals/Index', [
+        return Inertia::render('Jadwals/Index',[
             'thead' => $thead,
             'tbody' => $tbody,
         ]);
