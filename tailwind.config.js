fff397832959d840a4cf0985005a228b6e09f3e4
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-06-03 08:49:32 +0700

    refactor(welcome): adjust layout and tailwindcss utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,26 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
-            colors:{
-                'primary': '#AC1929',
-                'hover-primary': '#930010',
-
+            colors: {
+                primary: "#AC1929",
+                "hover-primary": "#930010",
             },
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, require("daisyui")],
 };
